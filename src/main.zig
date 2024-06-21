93a4a9bef90a6d7852e3d7073af31fbce7a83143
Guillaume <guillaume.thouvenin@vates.tech>
2024-06-21 18:17:27 +0200

    add comment (no new code)

--- src/main.zig
+++ src/main.zig
@@ -10,19 +10,6 @@ const f = @cImport({
 });
 
 pub fn main() !void {
-    // ----------------------- W A S M E R ------------------------------------
-    var func_args = std.process.args();
-    // We expect the WAT file name as the first parameter
-    // Skip the first args that is the name of the program
-    _ = func_args.skip();
-    const watf_opt = func_args.next();
-
-    if (watf_opt) |wat_filename| {
-        try wat.build_and_run_wat(wat_filename);
-    } else {
-        std.debug.print("WASM part skipped because no WAT filename provided\n", .{});
-    }
-
     // ----------------------- R A Y L I B ------------------------------------
     r.InitWindow(800, 600, "Raylib in Zig");
     while (!r.WindowShouldClose()) {
@@ -41,6 +28,20 @@ pub fn main() !void {
 
     r.CloseWindow();
 
+    // ----------------------- W A S M E R ------------------------------------
+    var func_args = std.process.args();
+    // We expect the WAT file name as the first parameter
+    // Skip the first args that is the name of the program
+    _ = func_args.skip();
+    const watf_opt = func_args.next();
+
+    if (watf_opt) |wat_filename| {
+        try wat.build_and_run_wat(wat_filename);
+    } else {
+        std.debug.print("WASM part skipped because no WAT filename provided\n", .{});
+    }
+
+    // ------------------ R U N  O U R  "C"  C O D E --------------------------
     const answer = f.foo(30, 12);
     std.debug.print("{d} is the answer to the Ultimate Question of Life, the Universe, and Everything", .{answer});
 }
